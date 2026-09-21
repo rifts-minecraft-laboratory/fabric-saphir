cbbfdd733e4d3da1561ac3b5dc26abbf95883a96
Noah Boos <noahboos-dev@proton.me>
2026-09-21 22:31:50 +0200

    Feature - Adjusted sapphire ore vein spawn rates.

--- src/main/java/fr/noahboos/saphir/worldgen/features/placed/definitions/OrePlacedFeatures.java
+++ src/main/java/fr/noahboos/saphir/worldgen/features/placed/definitions/OrePlacedFeatures.java
@@ -17,42 +17,88 @@ public class OrePlacedFeatures {
     public static void bootstrap(BootstrapContext<PlacedFeature> context) {
         HolderGetter<ConfiguredFeature<?, ?>> configuredFeatureHolderGetter = context.lookup(Registries.CONFIGURED_FEATURE);
 
-        List<PlacementModifier> oreSapphireVeinModifiers = List.of(
-            CountPlacement.of(200),
-            BiomeFilter.biome(),
-            InSquarePlacement.spread(),
-            HeightRangePlacement.of(
-                BiasedToBottomHeight.of(
-                    VerticalAnchor.BOTTOM,
-                    VerticalAnchor.absolute(16),
-                    4
-                )
-            )
-        );
-
         context.register(EXTRA_SMALL_SAPPHIRE_VEIN_PLACED_KEY, new PlacedFeature(
             configuredFeatureHolderGetter.getOrThrow(OreConfiguredFeatures.EXTRA_SMALL_SAPPHIRE_VEIN_CONFIGURED_KEY),
-            oreSapphireVeinModifiers
+            List.of(
+                CountPlacement.of(8),
+                BiomeFilter.biome(),
+                InSquarePlacement.spread(),
+                HeightRangePlacement.of(
+                    BiasedToBottomHeight.of(
+                        VerticalAnchor.BOTTOM,
+                        VerticalAnchor.absolute(16),
+                        4
+                    )
+                )
+            )
         ));
 
         context.register(SMALL_SAPPHIRE_VEIN_PLACED_KEY, new PlacedFeature(
             configuredFeatureHolderGetter.getOrThrow(OreConfiguredFeatures.SMALL_SAPPHIRE_VEIN_CONFIGURED_KEY),
-            oreSapphireVeinModifiers
+            List.of(
+                CountPlacement.of(6),
+                RarityFilter.onAverageOnceEvery(2),
+                BiomeFilter.biome(),
+                InSquarePlacement.spread(),
+                HeightRangePlacement.of(
+                    BiasedToBottomHeight.of(
+                        VerticalAnchor.BOTTOM,
+                        VerticalAnchor.absolute(16),
+                        4
+                    )
+                )
+            )
         ));
 
         context.register(MEDIUM_SAPPHIRE_VEIN_PLACED_KEY, new PlacedFeature(
             configuredFeatureHolderGetter.getOrThrow(OreConfiguredFeatures.MEDIUM_SAPPHIRE_VEIN_CONFIGURED_KEY),
-            oreSapphireVeinModifiers
+            List.of(
+                CountPlacement.of(4),
+                RarityFilter.onAverageOnceEvery(4),
+                BiomeFilter.biome(),
+                InSquarePlacement.spread(),
+                HeightRangePlacement.of(
+                    BiasedToBottomHeight.of(
+                        VerticalAnchor.BOTTOM,
+                        VerticalAnchor.absolute(16),
+                        4
+                    )
+                )
+            )
         ));
 
         context.register(LARGE_SAPPHIRE_VEIN_PLACED_KEY, new PlacedFeature(
             configuredFeatureHolderGetter.getOrThrow(OreConfiguredFeatures.LARGE_SAPPHIRE_VEIN_CONFIGURED_KEY),
-            oreSapphireVeinModifiers
+            List.of(
+                CountPlacement.of(4),
+                RarityFilter.onAverageOnceEvery(8),
+                BiomeFilter.biome(),
+                InSquarePlacement.spread(),
+                HeightRangePlacement.of(
+                    BiasedToBottomHeight.of(
+                        VerticalAnchor.BOTTOM,
+                        VerticalAnchor.absolute(16),
+                        4
+                    )
+                )
+            )
         ));
 
         context.register(EXTRA_LARGE_SAPPHIRE_VEIN_PLACED_KEY, new PlacedFeature(
             configuredFeatureHolderGetter.getOrThrow(OreConfiguredFeatures.EXTRA_LARGE_SAPPHIRE_VEIN_CONFIGURED_KEY),
-            oreSapphireVeinModifiers
+            List.of(
+                CountPlacement.of(2),
+                RarityFilter.onAverageOnceEvery(12),
+                BiomeFilter.biome(),
+                InSquarePlacement.spread(),
+                HeightRangePlacement.of(
+                    BiasedToBottomHeight.of(
+                        VerticalAnchor.BOTTOM,
+                        VerticalAnchor.absolute(16),
+                        4
+                    )
+                )
+            )
         ));
     }
 
