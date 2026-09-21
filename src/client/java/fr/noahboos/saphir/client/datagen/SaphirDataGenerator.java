0be3de90b5b5522a40fc0743f4c784f924246d9d
Noah Boos <noahboos-dev@proton.me>
2026-09-21 17:08:30 +0200

    Chore - Added the model provider class.

--- src/client/java/fr/noahboos/saphir/client/datagen/SaphirDataGenerator.java
+++ src/client/java/fr/noahboos/saphir/client/datagen/SaphirDataGenerator.java
@@ -1,11 +1,16 @@
-package fr.noahboos.saphir.client;
+package fr.noahboos.saphir.client.datagen;
 
+import fr.noahboos.saphir.Saphir;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class SaphirDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-		//
+		Saphir.LOGGER.info("Initializing {}'s datagen.", Saphir.MOD_ID);
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+		pack.addProvider(SaphirModelProvider::new);
+		Saphir.LOGGER.info("Initialized {}'s datagen.", Saphir.MOD_ID);
 	}
 }
---
+++ src/client/java/fr/noahboos/saphir/client/datagen/SaphirModelProvider.java
@@ -0,0 +1,32 @@
+package fr.noahboos.saphir.client.datagen;
+
+import fr.noahboos.saphir.Saphir;
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
+import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.minecraft.client.data.models.BlockModelGenerators;
+import net.minecraft.client.data.models.ItemModelGenerators;
+
+public class SaphirModelProvider extends FabricModelProvider {
+    public SaphirModelProvider(FabricPackOutput fabricPackOutput) {
+        super(fabricPackOutput);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockModelGenerators blockModelGenerators) {
+        Saphir.LOGGER.info("Generating {}'s block models.", Saphir.MOD_ID);
+        //
+        Saphir.LOGGER.info("Generated {}'s block models.", Saphir.MOD_ID);
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerators itemModelGenerators) {
+        Saphir.LOGGER.info("Generating {}'s item models.", Saphir.MOD_ID);
+        //
+        Saphir.LOGGER.info("Generated {}'s item models.", Saphir.MOD_ID);
+    }
+
+    @Override
+    public String getName() {
+        return "SaphirModelProvider";
+    }
+}
