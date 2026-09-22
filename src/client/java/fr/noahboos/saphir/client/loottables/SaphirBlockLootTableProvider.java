409d09b651aafb225342efc060c2a76898638e99
Noah Boos <noahboos-dev@proton.me>
2026-09-22 16:58:00 +0200

    Feature - Added drops to all previously added blocks.

--- src/client/java/fr/noahboos/saphir/client/loottables/SaphirBlockLootTableProvider.java
+++ src/client/java/fr/noahboos/saphir/client/loottables/SaphirBlockLootTableProvider.java
@@ -1,9 +1,10 @@
 package fr.noahboos.saphir.client.loottables;
 
+import fr.noahboos.saphir.blocks.SaphirBlocks;
+import fr.noahboos.saphir.items.SaphirItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootSubProvider;
 import net.minecraft.core.HolderLookup;
-
 import java.util.concurrent.CompletableFuture;
 
 public class SaphirBlockLootTableProvider extends FabricBlockLootSubProvider {
@@ -13,6 +14,10 @@ public class SaphirBlockLootTableProvider extends FabricBlockLootSubProvider {
 
     @Override
     public void generate() {
-        //
+        dropSelf(SaphirBlocks.BLUE_SAPPHIRE_BLOCK);
+        dropWhenSilkTouch(SaphirBlocks.BLUE_SAPPHIRE_ORE);
+        add(SaphirBlocks.BLUE_SAPPHIRE_ORE, createOreDrop(SaphirBlocks.BLUE_SAPPHIRE_ORE, SaphirItems.BLUE_SAPPHIRE));
+        dropWhenSilkTouch(SaphirBlocks.DEEPSLATE_BLUE_SAPPHIRE_ORE);
+        add(SaphirBlocks.DEEPSLATE_BLUE_SAPPHIRE_ORE, createOreDrop(SaphirBlocks.DEEPSLATE_BLUE_SAPPHIRE_ORE, SaphirItems.BLUE_SAPPHIRE));
     }
 }
