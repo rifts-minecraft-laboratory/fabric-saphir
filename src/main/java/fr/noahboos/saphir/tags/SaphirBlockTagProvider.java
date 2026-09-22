8459ce396e1a696d6dcb8cd89746827e3d7a1997
Noah Boos <noahboos-dev@proton.me>
2026-09-22 16:34:46 +0200

    Feature - Added current blocks to the `MINEABLE_WITH_PICKAXE` and to the `NEEDS_DIAMOND_TOOL` block tags.

--- src/main/java/fr/noahboos/saphir/tags/SaphirBlockTagProvider.java
+++ src/main/java/fr/noahboos/saphir/tags/SaphirBlockTagProvider.java
@@ -1,8 +1,10 @@
 package fr.noahboos.saphir.tags;
 
+import fr.noahboos.saphir.blocks.SaphirBlockItemIds;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.tags.BlockTags;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -13,6 +15,14 @@ public class SaphirBlockTagProvider extends FabricTagsProvider.BlockTagsProvider
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
-        //
+        builder(BlockTags.MINEABLE_WITH_PICKAXE)
+            .add(SaphirBlockItemIds.BLUE_SAPPHIRE_BLOCK)
+            .add(SaphirBlockItemIds.BLUE_SAPPHIRE_ORE)
+            .add(SaphirBlockItemIds.DEEPSLATE_BLUE_SAPPHIRE_ORE);
+
+        builder(BlockTags.NEEDS_DIAMOND_TOOL)
+            .add(SaphirBlockItemIds.BLUE_SAPPHIRE_BLOCK)
+            .add(SaphirBlockItemIds.BLUE_SAPPHIRE_ORE)
+            .add(SaphirBlockItemIds.DEEPSLATE_BLUE_SAPPHIRE_ORE);
     }
 }
