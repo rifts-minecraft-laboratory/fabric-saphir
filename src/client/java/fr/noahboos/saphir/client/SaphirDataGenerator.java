cc4591640b511b0de8c201309e008dc326abf43c
Noah Boos <noahboos-dev@proton.me>
2026-09-22 17:57:22 +0200

    Chore - Added item tag generation classes.

--- src/client/java/fr/noahboos/saphir/client/SaphirDataGenerator.java
+++ src/client/java/fr/noahboos/saphir/client/SaphirDataGenerator.java
@@ -4,6 +4,7 @@ import fr.noahboos.saphir.Saphir;
 import fr.noahboos.saphir.client.loottables.SaphirBlockLootTableProvider;
 import fr.noahboos.saphir.client.models.SaphirModelProvider;
 import fr.noahboos.saphir.tags.SaphirBlockTagProvider;
+import fr.noahboos.saphir.tags.SaphirItemTagProvider;
 import fr.noahboos.saphir.worldgen.features.configured.SaphirConfiguredFeatures;
 import fr.noahboos.saphir.worldgen.features.placed.SaphirPlacedFeatures;
 import fr.noahboos.saphir.worldgen.SaphirWorldgenProvider;
@@ -20,6 +21,7 @@ public class SaphirDataGenerator implements DataGeneratorEntrypoint {
 
 		pack.addProvider(SaphirBlockLootTableProvider::new);
 		pack.addProvider(SaphirBlockTagProvider::new);
+		pack.addProvider(SaphirItemTagProvider::new);
 		pack.addProvider(SaphirModelProvider::new);
 		pack.addProvider(SaphirWorldgenProvider::new);
 		Saphir.LOGGER.info("Initialized {}'s datagen.", Saphir.MOD_ID);
